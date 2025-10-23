3f2e4d9ac0468a623ccf958052fe50dae46826b9
Rafael Aznar <rafaaznar@gmail.com>
2025-10-23 12:02:34 +0200

    Add landing route to application routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
+import { Landing } from './components/landing/landing';
 
 export const routes: Routes = [
-    { path: 'home', component: Home }
+    { path: 'home', component: Home },
+    { path: 'landing', component: Landing }
 
 
 
