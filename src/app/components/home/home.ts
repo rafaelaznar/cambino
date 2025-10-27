fe175065429d6649042819289b3cf2634a3e6aff
Rafael Aznar <rafaaznar@gmail.com>
2025-10-27 12:14:29 +0100

    Enhance Home component: update HTML structure, add input handling, and improve CSS styles

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,8 +1,10 @@
+
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
   standalone: true
@@ -11,6 +13,9 @@ import { Component } from '@angular/core';
 export class Home {
 
   nombre = 'Rafa';
+  titulo = "Homer"
+  ancho = 300;
+  hasError= false;
 
   constructor() {
     console.log('constructor');
@@ -20,6 +25,26 @@ export class Home {
   ngOnInit() {
     console.log('ngOnInit');
     this.nombre = 'Ana';
+  }
+
+  noHayError() {
+    return this.hasError;
+  }
+
+  siHayError() {
+    return !this.hasError;
+  }
+
+  suma(a: number, b: number): number {
+    return a + b;
+  }
+
+  cambiarNombre() {
+    this.nombre = 'Juan';
+  }
+
+  cambiarNombreInput(event: any) {
+    this.nombre = event.target.value;
   }
 
 }
