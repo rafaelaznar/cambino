20c6272564a2d9f40dc71e9a16d8619717dba6d9
Rafael Aznar <rafaaznar@gmail.com>
2025-10-29 12:03:22 +0100

    Enhance Rxjscomponent: add new examples for RxJS observables and improve existing methods for better demonstration

--- src/app/components/rxjscomponent/rxjscomponent.ts
+++ src/app/components/rxjscomponent/rxjscomponent.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { concat, interval, merge, Observable, of, take } from 'rxjs';
 
 
 class Persona {
@@ -28,7 +28,7 @@ export class Rxjscomponent {
 
 
   nombres$: Observable<Persona[]> = of(this.listadoPersonas);
-  nombre$: Observable<Persona> = of(this.listadoPersonas[0]);  
+  nombre$: Observable<Persona> = of(this.listadoPersonas[0]);
   numero$: Observable<number> = of(Math.floor(Math.random() * 100) + 1);
 
 
@@ -84,7 +84,7 @@ export class Rxjscomponent {
 
   ejemplo05() {
     console.log('Ejecutando ejemplo 05 de RxJS: emisión de strings');
-
+    //observable
     const contador$ = new Observable<number>((observer) => {
       let count = 1;
       const interval = setInterval(() => {
@@ -96,7 +96,7 @@ export class Rxjscomponent {
         }
       }, 1000);
     });
-
+    //suscripción
     contador$.subscribe({
       next: (numero) => {
         console.log('Número recibido: ' + numero);
@@ -105,5 +105,88 @@ export class Rxjscomponent {
       complete: () => console.log('Flujo de datos completado.')
     });
   }
+
+  ejemplo06() {
+    console.log('Ejecutando ejemplo 06 de RxJS: emisión de strings');
+    //observable
+    const palabras$ = new Observable<string>((observer) => {
+      observer.next('Hola');
+      observer.next('que');
+      observer.next('tal');
+      observer.next('soy');
+      observer.next('rafa');
+      observer.complete();
+    });
+    //suscripción
+    palabras$.subscribe({
+      next: (palabra) => {
+        console.log('Palabra recibida: ' + palabra);
+      },
+      error: (err) => console.error('Error al recibir la palabra:', err),
+      complete: () => console.log('Flujo de datos completado.')
+    });
+  }
+
+  ejemplo07() {
+    console.log('Ejecutando ejemplo 07 de RxJS: emisión de strings con error');
+    //observable
+    const palabras$ = new Observable<string>((observer) => {
+      observer.next('Hola');
+      observer.next('que');
+      observer.next('tal');
+      observer.error('¡Ha ocurrido un error en la emisión de palabras!');
+      observer.next('soy');
+      observer.next('rafa');
+      observer.complete();
+    });
+    //suscripción
+    palabras$.subscribe({
+      next: palabra => console.log('Palabra recibida: ' + palabra),
+      error: err => console.error('Error al recibir la palabra:', err),
+      complete: () => console.log('Flujo de datos completado.')
+    });
+  }
+
+  ejemplo08() {
+    console.log('Ejecutando ejemplo 08 de RxJS: emisión con Observable.interval(10)');
+    //observable
+    const contador$ = interval(10);
+    //suscripción
+    const subscription = contador$.subscribe({
+      next: numero => console.log('Número recibido: ' + numero),
+      error: err => console.error('Error al recibir el número:', err),
+      complete: () => console.log('Flujo de datos completado.')
+    });
+    // Desuscribirse después de 5000 ms para detener la emisión
+    setTimeout(() => {
+      subscription.unsubscribe();
+      console.log('Desuscrito del observable de intervalo.');
+    }, 5000);
+  }
+
+
+  ejemplo09() {
+    console.log('Ejecutando ejemplo 09 de RxJS');
+    const intervalo1$ = interval(1000); //llega hasta el 5
+    const intervalo2$ = interval(1500); //llega hasta el 3
+    // combinar con el operador merge  los dos intervalos 
+    const combinado1$ = merge(intervalo1$, intervalo2$);
+    const combinado2$ = combinado1$.pipe(
+      take(10),      
+    );
+    combinado2$.subscribe({
+      next: numero => console.log('Número recibido: ' + numero),
+      error: err => console.error('Error al recibir el número:', err),
+      complete: () => console.log('Flujo de datos completado.')
+    });
+  }
+
+
+  // empezamos con el ejemplo del filter
+
+  // concepto de subject
+  // ejemplos de los distintos tipos de subject: subject, behaviorSubject, replaySubject, asyncSubject
+
+  
 
 }
