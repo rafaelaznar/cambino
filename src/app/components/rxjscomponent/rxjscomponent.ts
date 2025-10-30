b95525f70026d97178cd6890d8f818df6a85305b
Rafael Aznar <rafaaznar@gmail.com>
2025-10-30 08:59:08 +0100

    Enhance Rxjscomponent: add new examples for RxJS operators and improve existing methods for better demonstration

--- src/app/components/rxjscomponent/rxjscomponent.ts
+++ src/app/components/rxjscomponent/rxjscomponent.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { concat, interval, merge, Observable, of, take } from 'rxjs';
+import { BehaviorSubject, concat, filter, interval, map, merge, Observable, of, Subject, take } from 'rxjs';
 
 
 class Persona {
@@ -164,15 +164,19 @@ export class Rxjscomponent {
     }, 5000);
   }
 
-
+  // a partir de dos observables los combino con el merge y me suscribo al resultado
   ejemplo09() {
     console.log('Ejecutando ejemplo 09 de RxJS');
-    const intervalo1$ = interval(1000); //llega hasta el 5
-    const intervalo2$ = interval(1500); //llega hasta el 3
+    const intervalo1$ = interval(810); //llega hasta el 5
+    const intervalo2$ = interval(340); //llega hasta el 3
     // combinar con el operador merge  los dos intervalos 
     const combinado1$ = merge(intervalo1$, intervalo2$);
     const combinado2$ = combinado1$.pipe(
-      take(10),      
+      take(20),
+      // filtra todos los números pares
+      filter(numero => numero % 2 === 0),
+      // map para sumar 1 a cada número
+      map(numero => numero + 1)
     );
     combinado2$.subscribe({
       next: numero => console.log('Número recibido: ' + numero),
@@ -182,11 +186,72 @@ export class Rxjscomponent {
   }
 
 
-  // empezamos con el ejemplo del filter
+
+  // a partir de 1 observable nos suscribimos con dos suscriptores diferentes
+  ejemplo10() {
+    console.log('Ejecutando ejemplo 10 de RxJS');
+    const intervalo$ = interval(500);
+    const subscription1 = intervalo$.subscribe({
+      next: numero => console.log('Suscriptor 1 - Número recibido: ' + numero),
+      error: err => console.error('Suscriptor 1 - Error al recibir el número:', err),
+      complete: () => console.log('Suscriptor 1 - Flujo de datos completado.')
+    });
+    const subscription2 = intervalo$.subscribe({
+      next: numero => console.log('Suscriptor 2 - Número recibido: ' + numero),
+      error: err => console.error('Suscriptor 2 - Error al recibir el número:', err),
+      complete: () => console.log('Suscriptor 2 - Flujo de datos completado.')
+    });
+    // Desuscribirse después de 5000 ms para detener la emisión
+    setTimeout(() => {
+      subscription1.unsubscribe();
+      console.log('Desuscrito del observable de intervalo el subscriptor 1.');
+    }, 5000);
+    setTimeout(() => {
+      subscription2.unsubscribe();
+      console.log('Desuscrito del observable de intervalo el subscriptor 2.');
+    }, 8000);
+  }
+
+  // pendiente observables frios vs calientes
+
+
 
   // concepto de subject
-  // ejemplos de los distintos tipos de subject: subject, behaviorSubject, replaySubject, asyncSubject
 
-  
+  ejemplo11() {
+    console.log('Ejecutando ejemplo 11 de RxJS: Subject');
+    const subject = new Subject<string>();
+    subject.subscribe({
+      next: (value) => console.log('s1: Valor recibido en el subject: ' + value),
+      complete: () => console.log('s1: Flujo de datos completado en el subject.')
+    });
+    subject.next('Hola');
+    subject.next('Mundo');
+    subject.subscribe({
+      next: (value) => console.log('s2: Valor recibido en el subject: ' + value),
+      complete: () => console.log('s2: Flujo de datos completado en el subject.')
+    });
+    subject.next('¡Saludos desde el Subject!');
+    subject.complete();
+  }
+
+  ejemplo12() {
+    console.log('Ejecutando ejemplo 12 de RxJS: BehaviorSubject');
+    const subject = new BehaviorSubject<string>('Valor inicial');
+    subject.subscribe({
+      next: (value) => console.log('s1: Valor recibido en el subject: ' + value),
+      complete: () => console.log('s1: Flujo de datos completado en el subject.')
+    });
+    subject.next('Hola');
+    subject.next('Mundo');
+    subject.subscribe({
+      next: (value) => console.log('s2: Valor recibido en el subject: ' + value),
+      complete: () => console.log('s2: Flujo de datos completado en el subject.')
+    });
+    subject.next('¡Saludos desde el Subject!');
+    subject.complete();
+  }
+
+  // ejemplos de los distintos tipos de subject: *subject, behaviorSubject, replaySubject, asyncSubject
 
 }
