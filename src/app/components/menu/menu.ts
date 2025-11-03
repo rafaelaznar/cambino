8e84293e50e392c863712266e3448eabd4b10a96
Rafael Aznar <rafaaznar@gmail.com>
2025-11-03 11:44:21 +0100

    Implement dynamic active route highlighting in menu component

--- src/app/components/menu/menu.ts
+++ src/app/components/menu/menu.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -7,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './menu.css',
 })
 export class Menu {
+
+  activeRoute: string = '';
+
+  constructor(private oRouter: Router) {
+    // obtener la ruta activa a partir del router
+    this.oRouter.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.activeRoute = event.url;
+      }
+    });
+  }
+
 
 }
